813b9bf560903ad0b594acda5e3cf8a933984721
Gokulakrishnan <95609182+Gocool07@users.noreply.github.com>
2022-01-12 22:44:43 +0530

    Add files via upload

---
+++ A2(11.01.22 )/add/add.java
@@ -0,0 +1,10 @@
+class add
+{
+public static void main(String args[])
+{
+int a=5;
+double b=7.08;
+double c=a+b;
+System.out.println("Add of two value="+c);
+}
+}
---
+++ A2(11.01.22 )/multi/multi.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class multi
+{
+public static void main(String args[])
+{
+int b,c,d;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter any Number::");
+int a=sc.nextInt();
+b=a*a;
+c=a*a*a;
+d=a*a*a*a;
+System.out.println("Square of a ="+b);
+System.out.println("Cube of a ="+c);
+System.out.println("Fourth power of a ="+d);
+}
+}
