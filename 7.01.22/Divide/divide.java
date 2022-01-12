dc2bf1cd41a3c1d62ddff527770d40627b776293
Gokulakrishnan <95609182+Gocool07@users.noreply.github.com>
2022-01-12 22:35:15 +0530

    Add files via upload

---
+++ 7.01.22/Divide/divide.java
@@ -0,0 +1,8 @@
+class divide
+{
+public static void main(String args[])
+{
+int x=23,y=2,z=x/y;
+System.out.println("Divide of two Numbers ::"+z);
+}
+}
---
+++ 7.01.22/SUM/sum.java
@@ -0,0 +1,8 @@
+class sum
+{
+public static void main(String args[])
+{
+int x=23,y=2,z=x+y;
+System.out.println("Sum of two Numbers ::"+z);
+}
+}
