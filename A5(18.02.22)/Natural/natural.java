d1e3ed4f181f16806fbeceb870d8425df0bbae4e
Gokulakrishnan <95609182+Gocool07@users.noreply.github.com>
2022-01-19 22:18:37 +0530

    Add files via upload

---
+++ A5(18.02.22)/Natural/natural.java
@@ -0,0 +1,11 @@
+import java.util.*;
+package p;
+class natural
+
+{
+public void m()
+{
+System.out.println("Natural Numbers is:"+i);
+}
+}
+
---
+++ A5(18.02.22)/greater/greater.java
@@ -0,0 +1,22 @@
+class greater
+{
+public static void main(String args[])
+{
+int a=45,b=100,c=80;
+if(a>b)
+{
+	if(a>c)
+	{
+	System.out.println(a);
+	}
+	else
+	{
+		System.out.println(c);
+	}
+}
+else
+{
+	System.out.println(b);
+}
+}
+}
