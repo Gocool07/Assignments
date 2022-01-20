6455917017730d8f8619283fa5c6bd2d7c9f365e
Gokulakrishnan <95609182+Gocool07@users.noreply.github.com>
2022-01-20 10:38:55 +0530

    Add files via upload

---
+++ A4(17.01.22)/Signed/signed.java
@@ -0,0 +1,12 @@
+class signed
+{
+public static void main(String args[])
+{
+System.out.println(10>>2);
+System.out.println(-10>>2);
+System.out.println(10<<2);
+System.out.println(-10<<2);
+System.out.println(10>>>2);
+System.out.println(-20>>>2);
+}
+}
